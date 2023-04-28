483d68e1df4631b459e1a0e1363256c98be00b47
Max Sauerbrey <maxsauerbrey@gmail.com>
2023-04-28 00:28:03 -0300

    first commit

---
+++ src/main/java/com/example/registration_backend_email_verification/RegistrationBackendEmailVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.registration_backend_email_verification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationBackendEmailVerificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationBackendEmailVerificationApplication.class, args);
+	}
+
+}
