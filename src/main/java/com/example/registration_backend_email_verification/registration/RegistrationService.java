cedc467944c1bcdccf0be461411793e18b540a4b
Max Sauerbrey <maxsauerbrey@gmail.com>
2023-04-28 17:48:18 -0300

    add RegistrationService

---
+++ src/main/java/com/example/registration_backend_email_verification/registration/RegistrationService.java
@@ -0,0 +1,7 @@
+package com.example.registration_backend_email_verification.registration;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
+}
