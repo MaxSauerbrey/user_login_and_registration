4df14261daaeac6e9c7be435a0f44a85b3fca18a
Max Sauerbrey <maxsauerbrey@gmail.com>
2023-04-30 01:15:15 -0300

    add AppUserRepository

--- src/main/java/com/example/registration_backend_email_verification/appuser/repository/AppUserRepository.java
+++ src/main/java/com/example/registration_backend_email_verification/appuser/repository/AppUserRepository.java
@@ -1,13 +1,14 @@
 package com.example.registration_backend_email_verification.appuser.repository;
 
 import com.example.registration_backend_email_verification.appuser.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.util.Optional;
 @Repository
 @Transactional
-public interface AppUserRepository {
+public interface AppUserRepository extends JpaRepository <AppUser, Long> {
     Optional<AppUser> findByEmail (String email);
 
 }
