437f83479f0a7b869f648494dd68e7ee77ba84ee
Max Sauerbrey <maxsauerbrey@gmail.com>
2023-04-30 04:39:14 -0300

    add import appUser in RegistrationService

--- src/main/java/com/example/registration_backend_email_verification/registration/RegistrationService.java
+++ src/main/java/com/example/registration_backend_email_verification/registration/RegistrationService.java
@@ -1,10 +1,26 @@
 package com.example.registration_backend_email_verification.registration;
 
+import com.example.registration_backend_email_verification.appuser.AppUser;
+import com.example.registration_backend_email_verification.appuser.AppUserRole;
+import com.example.registration_backend_email_verification.appuser.service.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private final EmailValidator emailValidator;
+    private final AppUserService appUserService;
     public String register(RegistrationRequest request) {
-        return "it works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if(!isValidEmail) throw new IllegalStateException("email not valid");
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER));
     }
 }
