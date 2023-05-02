898b9242894b492987bb96d9719b063973145486
Max Sauerbrey <maxsauerbrey@gmail.com>
2023-05-02 16:29:45 -0300

    add findByEmail method

--- src/main/java/com/example/registration_backend_email_verification/appuser/repository/AppUserRepository.java
+++ src/main/java/com/example/registration_backend_email_verification/appuser/repository/AppUserRepository.java
@@ -2,6 +2,8 @@ package com.example.registration_backend_email_verification.appuser.repository;
 
 import com.example.registration_backend_email_verification.appuser.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
@@ -10,5 +12,10 @@ import java.util.Optional;
 @Transactional
 public interface AppUserRepository extends JpaRepository <AppUser, Long> {
     Optional<AppUser> findByEmail (String email);
+    @Transactional
+    @Modifying
+    @Query("UPDATE AppUser a " +
+            "SET a.enabled = TRUE WHERE a.email = ?1")
+    int enableAppUser(String email);
 
 }
